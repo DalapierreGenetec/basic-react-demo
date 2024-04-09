fff070a9a0fe29391001a090956baa4627d5d444
David-Alexandre Lapierre <dalapierre@genetec.com>
2024-04-09 17:34:00 -0400

    working on styling or navbar

--- src/modules/top-nav/index.tsx
+++ src/modules/top-nav/index.tsx
@@ -1,5 +1,6 @@
 import { TopNavProps } from "./topNav.props";
 import { TopNavTemplate } from "./topNav.template"
+import './topNav.styles.css';
 
 export const TopNav = (props: TopNavProps) => {
     return TopNavTemplate(props.navItems);
--- src/modules/top-nav/topNav.template.tsx
+++ src/modules/top-nav/topNav.template.tsx
@@ -1,13 +1,13 @@
 import { TopNavItem } from "@portal/libs/top-nav/topNavItem";
 
-const TopNavItemTemplate = (item: TopNavItem) => (
-    <div key={item.display}>
+const TopNavItemTemplate = (item: TopNavItem, index: number) => (
+    <div key={item.display} className={index === 0 ? "selected top-nav-item" : "top-nav-item"}>
         {item.display}
     </div>
 );
 
 export const TopNavTemplate = (navItems: TopNavItem[]) => (
     <div id="top-nav-bar-container">
-        {navItems.map((item) => TopNavItemTemplate(item))}
+        {navItems.map((item, i) => TopNavItemTemplate(item, i))}
     </div>
 );
