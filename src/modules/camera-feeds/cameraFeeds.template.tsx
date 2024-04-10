5d1cf7b1a2c952677dd7138dbb88c96392a617fc
David-Alexandre Lapierre <dalapierre@genetec.com>
2024-04-10 10:16:20 -0400

    added another button

--- src/modules/camera-feeds/cameraFeeds.template.tsx
+++ src/modules/camera-feeds/cameraFeeds.template.tsx
@@ -1,5 +1,8 @@
-export const CameraFeedsTemplate = () => (
+import { ButtonTest } from "@portal/components/button-test";
+
+export const CameraFeedsTemplate = (testCallback: () => void) => (
     <div id="camera-feeds">
         <p>Hello camera feeds</p>
+        <ButtonTest display="Another press me" onClickCallback={testCallback} />
     </div>
 );
--- src/modules/camera-feeds/index.tsx
+++ src/modules/camera-feeds/index.tsx
@@ -2,5 +2,10 @@ import { CameraFeedsTemplate } from "./cameraFeeds.template"
 import './cameraFeeds.styles.css';
 
 export const CameraFeeds = () => {
-    return CameraFeedsTemplate();
+
+    const testCallback2 = () => {
+        console.log('Test callback 2 succeeded');
+    }
+
+    return CameraFeedsTemplate(testCallback2);
 }
